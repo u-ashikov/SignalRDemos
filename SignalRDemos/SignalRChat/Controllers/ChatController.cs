e4e7d726ee0fa34a168544f0452533807c34fbef
u.ashikov <u.ashikov@abv.bg>
2019-12-13 23:30:36 +0200

    Chat demo - initial project setup.

---
+++ SignalRDemos/SignalRChat/Controllers/ChatController.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRChat.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ChatController : Controller
+    {
+        public IActionResult ChatRoom() => this.View();
+    }
+}
---
+++ SignalRDemos/SignalRChat/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        protected readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Chat", action = "ChatRoom" });
+            });
+        }
+    }
+}
