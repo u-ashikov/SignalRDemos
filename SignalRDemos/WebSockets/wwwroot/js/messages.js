7f6c8aa32a874fa8c406a6202f83324d17f419e4
u.ashikov <u.ashikov@abv.bg>
2019-12-18 21:31:22 +0200

    Added check health function to enable/disable connect/close connection buttons.

--- SignalRDemos/WebSockets/wwwroot/js/messages.js
+++ SignalRDemos/WebSockets/wwwroot/js/messages.js
@@ -9,10 +9,14 @@ $('#connect').on('click', function (event) {
 
     socket.onopen = function (event) {
         console.log('Socket opened!');
+
+        ckeckHealth();
     };
 
     socket.onclose = function (event) {
         console.log('Socket closed!');
+
+        ckeckHealth();
     };
 
     socket.onmessage = function (event) {
@@ -25,13 +29,33 @@ $('#connect').on('click', function (event) {
 });
 
 $('#close').on('click', function (event) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert('Socket not connected!');
+    }
+
     socket.close(1000, 'Closed from client!');
 });
 
 $('#send-btn').on('click', function (event) {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        alert('Socket not connected!');
+    }
+
     var message = $('#message').val();
 
     socket.send(message);
 
     $('#messages').append($('<li>').text(`Message from client: ${message}`));
 });
+
+function ckeckHealth() {
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        $('#send-btn, #close').attr('disabled', 'disabled');
+        $('div#socket-status').text('Closed');
+    } else {
+        $('#send-btn, #close').removeAttr('disabled', 'disabled');
+        $('div#socket-status').text('Connected');
+    }
+}
+
+ckeckHealth();
